67f955cef3257ae198e6d0b3150558fce4295585
Mohamed Shadab <shadabdragon@gmail.com>
2019-07-03 21:22:11 +0400

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# 👓 mail-goggles-frontend
+
+Frontend for an email tracker built with VueJS.
+
+## Development
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="app-title">Mail Goggles</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "👓 Mail Goggles"
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:800&display=swap");
+
+body {
+  background: linear-gradient(180deg, #3f2b96 0%, #5484ff 100%);
+  height: 100vh;
+  color: azure;
+}
+
+.app-title {
+  padding-top: 30px;
+  font-family: "Montserrat", sans-serif;
+  color: azure;
+  font-size: 60px;
+  text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.1);
+}
+</style>
